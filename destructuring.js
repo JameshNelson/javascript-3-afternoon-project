f690b5c7529c67dafa360129f43e90509f222e24
James Nelson <johndoe@example.com>
2019-11-15 14:22:33 -0700

    finished da stuff

--- destructuring.js
+++ destructuring.js
@@ -20,8 +20,9 @@ var carDetails = {
 /*
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
+let {color, make, model, year} = carDetails; 
 
-//Code Here
+
 
 
 
@@ -34,7 +35,7 @@ var carDetails = {
 */
 
 function greeting( obj ) {
-  //Code Here
+  let {firstName, lastName, title} = obj; 
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -53,7 +54,12 @@ function greeting( obj ) {
   Sum up the values and return the total number.
 */
 
-//Code Here
+function totalPopulation(obj){
+  let {utah, california, texas, arizona} = obj; 
+  return utah + california + texas + arizona
+}
+ 
+
 
 
 
@@ -67,7 +73,11 @@ function greeting( obj ) {
   Push these new variables to an array and return the array. 
 */
 
-//Code Here
+function ingredients(obj){
+  let {carb, fat, protein} = obj; 
+  let newArr = [carb, fat, protein];
+  return newArr; 
+}
 
 
 
@@ -85,7 +95,14 @@ function greeting( obj ) {
   Find the smallest number of the three and return that number.
 */
 
-//Code Here
+function largeNumbers({first, second, third}){
+  var smallest
+  second < first && second < third ? smallest = second :
+  third < first && third < third ? smallest = third: 
+  smallest = first; 
+  return smallest; 
+
+}
 
 
 
@@ -97,6 +114,17 @@ function greeting( obj ) {
   Find the longest array and return that array.
 */
 
-//Code Here
+function numberGroups({a,b,c}){
+  var longest = a
+  if (b.length > a.length && b.length > c.length){
+    return longest = b; 
+  }
+  else if(c.length > a.length && c.length > b.length){
+    return longest = c; 
+  }
+  else {
+    return longest; 
+  }
+}
 
 
--- nesting.js
+++ nesting.js
@@ -50,7 +50,18 @@ var employees = [
     3. Return the updated employee array.
 */
 
-//Code Here
+let employeeUpdater = () =>{
+  for(i = 0; i < employees.length; i++){
+   if(employees[i].firstName === "Theo"){
+     employees.splice([i],1); 
+   }
+   if(employees[i].firstName === "Lorie"){
+     employees[i].department = "HR"
+   }
+
+  } 
+  return employees; 
+}
 
 
 
@@ -68,7 +79,18 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
     3. Return the updated array.
 */
 
-//Code Here
+let removeDuplicates = () =>{
+  for(i = 0; i < workplaceAccidents.length; i++){
+    for(j = i + 1;j< workplaceAccidents.length; j++){
+      if(workplaceAccidents[i] === workplaceAccidents[j]){
+        workplaceAccidents.splice([j],1); 
+      }
+    }
+  }
+  return workplaceAccidents
+}
+
+
 
 
 
@@ -97,8 +119,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1]; 
+var fluffy2ndFriend = cat.catFriends[1].name; 
 
 
 
@@ -138,8 +160,14 @@ var myCar = {
     3. Change atFaultForAccident from true to false.
 */
 
-//Code Here
-
+let recordCleaner = () =>{
+  for(i = 0; i < myCar.accidents.length; i++){
+    if(myCar.accidents[i].atFaultForAccident === true){
+      myCar.accidents[i].atFaultForAccident = false; 
+    }
+  }
+  return myCar; 
+  }
 
 
 ////////// PROBLEM 5 //////////
@@ -157,6 +185,16 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-//Code Here
+let looper = () =>{
+  for(i = 0; i < numsArr.length; i++){
+    for(j = 0; j < numsArr[i].length; j++){
+      if(numsArr[i][j] % 2 === 0){
+        numsArr[i][j] = "even";
+      }
+      else {numsArr[i][j] = 'odd'}
+    }
+  }
+  return numsArr; 
+}
 
 
